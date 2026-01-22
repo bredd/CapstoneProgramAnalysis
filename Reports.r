4b55712443fb8b6e66b90d1102056d1f83ce61b6
Brandt Redd <brandt@redd.org>
2026-01-22 11:06:20 -0700

    Set up reports in Latex format.

---
+++ Reports.r
@@ -0,0 +1,39 @@
+source("LoadData.r")
+source("AnalysisFunctions.r")
+
+counts <- CountColumn(data, "category");
+
+hc <- CrosstabColumns(data, "category", "hasCapstone");
+hc <- hc[, c("Yes", "No")]
+ReportCorrelationToLatex(counts, hc);
+
+ct.securityCourse <- CrosstabColumns(data, "category", "Security.Course.in.Program");
+ct.securityCourse <- ct.securityCourse[, c("Required", "Elective", "None")];
+ReportCorrelationToLatex(counts, ct.securityCourse);
+
+#z <- DropColumn(z, "No");
+#q <- cbind(Count = q, HasCapstone = z)
+#z <- CrosstabMultivalued(data, "category", "CAE");
+#q <- cbind(q, z);
+#q <- AddAllRow(q);
+#q <- AddFractionColumns(q);
+
+#print(q)
+
+#q <- CountColumn(data, "category");
+#z <- CrosstabColumns(data, "category", "Security.Course.in.Program");
+#q <- cbind(Count = q, z);
+#q <- AddAllRow(q);
+#q <- AddFractionColumns(q);
+
+#print(q)
+
+#q <- CountColumn(data, "category");
+#z <- CrosstabMultivalued(data, "category", "Cybersecurity.Program");
+#q <- cbind(Count = q, z);
+#q <- AddAllRow(q);
+#q <- AddFractionColumns(q);
+
+#print(q)
+
+# Next step - create report functions print reports by percentages in both text and LaTeX formats.
